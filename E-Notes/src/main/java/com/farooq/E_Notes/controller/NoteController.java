7a0b8402fa29d52dba59d2dc4343732223935abc
mohammadfarooq <farooq208019@gmail.com>
2024-10-18 19:56:05 +0530

    controller service

--- E-Notes/src/main/java/com/farooq/E_Notes/controller/NoteController.java
+++ E-Notes/src/main/java/com/farooq/E_Notes/controller/NoteController.java
@@ -1,14 +1,12 @@
 package com.farooq.E_Notes.controller;
 
 import com.farooq.E_Notes.model.Note;
+import com.farooq.E_Notes.model.dto.NoteRequest;
 import com.farooq.E_Notes.services.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -48,8 +46,47 @@ public class NoteController {
              return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
          }
     }
+    @PostMapping
+    public ResponseEntity<Note> add(@RequestBody Note addNote){
+        try{
+            _noteService.add(addNote);
+            return new ResponseEntity<>(addNote,HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
+    @PutMapping("{id}")
+    public ResponseEntity<Note> update(@PathVariable String id,@RequestBody Note updateNote){
+        try{
 
+            Note old = _noteService.getById(id).orElse(null);
+            if(old!=null){
+                _noteService.update(old,updateNote);
+                return new ResponseEntity<>(old, HttpStatus.OK);
+            }
+
+            else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<?> delete(@PathVariable String id){
+        try{
+            Note note = _noteService.getById(id).orElse(null);
+            if(note != null){
+                _noteService.delete(id);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
 
 
 }
